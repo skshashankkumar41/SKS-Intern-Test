fa13493664afdcddb6d254a45cfa92c4fad880e4
Shashank Prasad <skshashankkumar41@gmail.com>
2020-09-25 23:17:03 +0530

    dummy accuracy added

--- exercise-1.py
+++ exercise-1.py
@@ -11,4 +11,42 @@ def getFaceValue(string):
         
     return value
 
-print(getFaceValue("SAVE $1.00 ON TWO when you buy TWO BOXES (8.9 - 15.2 OZ. EXCLUDES LARGE SIZE) any flavor/variety General Mills cereal listed: *"))
+def reAccuracySign(dfPath):
+    df = pd.read_csv(dfPath,header = None)
+    actual = 0
+    pred = 0
+    for i in df.iloc[:,0]:
+        if ('$' or '¢') in i.lower():
+            actual += 1
+            if getFaceValue(i) != '':
+                pred += 1
+
+    return pred/actual 
+
+def reAccuracySave(dfPath):
+    df = pd.read_csv(dfPath,header = None)
+    actual = 0
+    pred = 0
+    for i in df.iloc[:,0]:
+        if ('save' or 'off' or 'max savings') in i.lower():
+            actual += 1
+            if getFaceValue(i) != '':
+                pred += 1
+
+    return pred/actual 
+
+def reAccuracyTotal(dfPath):
+    df = pd.read_csv(dfPath,header = None)
+    actual = df.shape[0]
+    pred = 0
+    for i in df.iloc[:,0]:
+        if getFaceValue(i) != '':
+            pred += 1
+
+    return pred/actual 
+
+print(reAccuracySave('coupons_ner.csv'))
+print(reAccuracySign('coupons_ner.csv'))
+print(reAccuracyTotal('coupons_ner.csv'))
+
+# print(getFaceValue("SAVE $1.00 ON TWO when you buy TWO BOXES (8.9 - 15.2 OZ. EXCLUDES LARGE SIZE) any flavor/variety General Mills cereal listed: *"))
